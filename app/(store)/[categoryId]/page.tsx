bab2370c752e7d91b31aa82820b6e4488f496236
RodsmonsterPC <rodofochido@gmail.com>
2025-04-01 12:01:09 -0600

    Avances de ctageorias y productos

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import ProductCard from "@/components/products/ProductCard"
+import { CategoryWithProductsResponseSchema } from "../../../src/schemas"
+import { redirect } from "next/navigation"
 
-const StorePage = () => {
+
+type Params = Promise<{categoryId: string}>
+
+async function getProducts(categoryId: string ){
+  const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
+
+  const req = await fetch(url)
+  const json = await req.json()
+  if(!req.ok){
+    redirect('/1')
+  }
+  const products = CategoryWithProductsResponseSchema.parse(json)
+  return products
+}
+
+const StorePage = async ({params} : {params: Params}) => {
+
+  const {categoryId} = await params
+
+
+  const category = await getProducts(categoryId)
+
+
     return (
-      <div>
-        storePage
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl-grid-cols-3">
+       {category.products.map(product => (
+        <ProductCard
+        key={product.id}
+        product={product}
+        />
+       ))}
       </div>
     )
   }
---
+++ components/cart/ShoppingCart.tsx
@@ -0,0 +1,12 @@
+
+
+const ShoppingCart = () => {
+  return (
+   <>
+   <h2 className="text-4xl font-bold text-gray-900">Resumen de Venta</h2>
+   
+   </>
+  )
+}
+
+export default ShoppingCart
